c60557f99973acd6a594f9c96686095469e2b4d0
AmarKolipe <87370429+AmarKolipe@users.noreply.github.com>
2022-03-25 17:23:25 +0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,29 @@
+var score = 0;
+
+function update_score()
+{
+
+    score = score + 1;
+
+    document.getElementById("score").innerHTML = "score: " + score;
+
+
+
+};
+
+function save_score()
+{
+
+  localStorage.setItem("score", score);
+
+
+
+};
+
+function next_page()
+{
+
+    window.location = "login.html";
+
+}
+
